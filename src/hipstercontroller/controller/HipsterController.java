cdbf5e40c3b6346458e4c9a576a0d3e11e465702
joshR2 <josh2cool365@aol.com>
2015-11-17 09:47:48 -0700

    got rid of a extra line of space.

--- src/hipstercontroller/controller/HipsterController.java
+++ src/hipstercontroller/controller/HipsterController.java
@@ -4,7 +4,9 @@ public class HipsterController
 {
 	public void start()
 	{
-		
+		String myName = baseFrame.getResponse("What is your name?");
+		firstHipster.setName(myName);
+		baseFrame.setTitle(myName + "'s Hipster Project!");
 	}
 
 }
